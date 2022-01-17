a7532bbeb98a6ec188afa2417a61be126c24c7e1
Andres Diaz <andiazvar@gmail.com>
2022-01-16 20:26:40 -0500

    temple sun and main character

---
+++ FirstProject/Intermediate/Build/Win64/FirstProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30139
+#include "C:/Users/zuly-/OneDrive/Documentos/UnrealEngneCourse/UnrealUltimateGameDevCourse/FirstProject/Intermediate/Build/Win64/FirstProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
